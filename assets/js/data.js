f78675f1adbd18500edcbc3268b7afb9a8f4eabe
gaoxiang <gx625888@gmail.com>
2019-04-22 11:47:59 +0800

    一

---
+++ assets/js/data.js
@@ -0,0 +1,12 @@
+//首页-联系我们
+$('.telphone_data').html("+86 13670187895");
+$('.email_data').html("88003635@qq.com");
+$('.address_data').html("深圳市南山区高新区南区粤兴一道8号 香港城市大学产学研大楼2楼");
+//通用配置
+$('.beianbianhao').html("© 2019 yuxuan Technology Limited.     |    粤ICP备19013413号");
+
+//banner
+$('#carousel-banner-1').attr('src','images/banner/1.jpeg');
+
+//新闻中心-新闻一
+$('#newscentre1_img1').attr('src','../../images/banner.jpg');
